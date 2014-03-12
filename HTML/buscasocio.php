cfadd2af3ff6912454e9963d14dbf93874bbb54f
davmunmoy <damu-cazalla@hotmail.es>
2014-03-12 09:25:28 +0100

    12/3/14
    buscasocio y puntuaciones acabadas

---
+++ HTML/buscasocio.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+	<meta name="description" content="Videoclub ORI">
+	<meta name="keywords" content="videoclub, ori, peliculas">
+	<title>Videoclub ORI</title>
+	<link rel="stylesheet" href="../css/general.css">
+	<link rel="stylesheet" href="../css/buscasocio.css">
+</head>
+<body>
+	
+		<header id="cabecera">
+			<h1>Videoclub ORI</h1>
+		</header>
+		<div id="cuerpo">
+		<nav id="navegador">
+			<?php
+				include "menus.php";
+				include "conexion.php";
+				Navegador();
+			?>
+		</nav>
+		<section id="seccion">
+			<article>
+			<h2> Usuarios</h2>
+			<form id="formulario" method="get" action="buscasocio.php" enctype="application/x-www-form-urlencoded">
+				
+				<?php
+						
+						$busqueda = "";						
+						if(isset($_GET['busqueda'])){
+							$busqueda = $_GET['busqueda'];
+							
+						}
+						echo '
+						<div class="search">
+							<input type="search" name="busqueda" value="'.$busqueda.'"/>
+							<input type="submit" value="Buscar" />
+						</div>';						
+						
+				?>
+					
+			</form>
+			</article>
+			<article>
+				<?php
+							if(isset($_GET['busqueda'])){
+								//Iniciacion de variables
+								$con = CrearConexionBD();
+								$cad = urldecode($_GET['busqueda']);											
+																
+								$sql = "select dni, nombre from socios where 
+											upper(nombre) like upper('%".$cad."%')";
+								
+								
+								foreach ( $con->query($sql) as $fila) {
+										echo '<ul class="iz">
+												
+												<li>
+													<a href="perfil.php?dni='.$fila[0].'"><img src="../img_socios/'.$fila[0].'" />
+													<figcaption class="nombre">'.$fila[1].'</figcaption>
+													
+												</li>											
+																								
+											</ul>';
+										
+									
+								}						
+																
+								CerrarConexionBD($con);
+							}else{
+								echo '</table>';
+							}
+							
+						?>
+				
+			</article>	
+		</section>
+		<aside id="menu">
+			<?php Menu(); ?>
+		</aside>
+		<footer id="pie">
+			Derechos Reservados &copy; 2013-2014
+		</footer>
+	</div>
+</body>
+</html>
---
+++ HTML/puntuaciones.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+	<meta name="description" content="Videoclub ORI">
+	<meta name="keywords" content="videoclub, ori, peliculas">
+	<title>Videoclub ORI</title>
+	<link rel="stylesheet" href="../css/general.css">
+	<link rel="stylesheet" href="../css/puntuaciones.css">
+</head>
+<body>
+	
+		<header id="cabecera">
+			<h1>Videoclub ORI</h1>
+		</header>
+		<div id="cuerpo">
+		<nav id="navegador">
+			<?php
+				include "menus.php";
+				Navegador();
+			?>
+		</nav>
+		<section id="seccion">
+			
+			<article>
+				<h2> Mis Puntuaciones</h2>
+				
+						
+						<?php
+							
+								include "conexion.php";
+								$con = CrearConexionBD();
+								
+								$dni = $_SESSION['dni'];
+								
+								$sql="select id_pelicula,id_pelicula_a_nombre(id_pelicula),estrellas from puntuaciones_peliculas where dni = '$dni'";
+								
+								foreach ($con->query($sql) as $fila) {
+									
+										echo '<ul class="iz">
+										
+												<li>
+													<img src="../img_peliculas/'.$fila[0].'" />
+													<figcaption class="nombre">'.$fila[1].'</figcaption>
+            										<p class = "punt">'.$fila[2].'</p>
+												</li>
+												
+												
+											</ul>';
+									
+								}								
+								echo '</table>';
+								
+								CerrarConexionBD($con);
+							
+							
+						?>
+			</article>	
+		</section>
+		<aside id="menu">
+			<?php Menu(); ?>
+		</aside>
+		<footer id="pie">
+			Derechos Reservados &copy; 2013-2014
+		</footer>
+	</div>
+</body>
+</html>
+
